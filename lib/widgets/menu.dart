8ea577132bef623b30c1dac07a7c4413f58d6f45
Alejandro González Reyes <alexglzrys@gmail.com>
2024-04-22 13:09:01 -0600

    mejorar la estructura del menú

--- lib/widgets/menu.dart
+++ lib/widgets/menu.dart
@@ -48,30 +48,65 @@ class Menu extends StatelessWidget {
         width: 220,
         height: 60,
         padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 4),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(30),
-          // Sombra
-          boxShadow: [
-            BoxShadow(
-                color: Colors.black.withOpacity(0.3),
-                offset: const Offset(0, 0),
-                blurRadius: 10),
-          ],
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          // Por cada item en el listado de opciones de menú, generar un IconButton con el icono y callback deinido para ese item
-          children: items
-              .map((e) => IconButton(
-                  onPressed: () => e.callback(),
-                  icon: Icon(
-                    e.icon,
-                    color: Colors.grey[700],
-                  )))
-              .toList(),
-        ),
+        decoration: decorationMenu(),
+        child: _MenuItems(items: items),
       ),
     );
+  }
+
+  // Método encargado de estableer la decoración del menú
+  BoxDecoration decorationMenu() {
+    return BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(30),
+      // Sombra
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.3),
+          offset: const Offset(0, 0),
+          blurRadius: 10,
+          // Aplicar efecto de iluminación en lugar de una sombra proyectada
+          // spreadRadius define la distancia a la que se extiende la sombra desde el objeto en todas las direcciones.
+          spreadRadius: -3,
+        ),
+      ],
+    );
+  }
+}
+
+// Widget privado encargado de definir el espacio u orientación de los elementos del menú
+class _MenuItems extends StatelessWidget {
+  const _MenuItems({
+    super.key,
+    required this.items,
+  });
+
+  final List<PinterestButton> items;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      // Por cada item en el listado de opciones de menú, generar un IconButton con el icono y callback deinido para ese item
+      children: items.map((item) => _MenuButton(item: item)).toList(),
+    );
+  }
+}
+
+// Widget privado que define un botón como opción del menú
+class _MenuButton extends StatelessWidget {
+  const _MenuButton({
+    super.key,
+    required this.item,
+  });
+  final PinterestButton item;
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+        onPressed: () => item.callback(),
+        icon: Icon(
+          item.icon,
+          color: Colors.blueGrey,
+        ));
   }
 }
